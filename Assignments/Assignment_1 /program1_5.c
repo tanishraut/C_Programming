0db8b98d5f086a6df67a1a7796aad4858397410e
Tanish Raut <165566146+tanishraut@users.noreply.github.com>
2025-10-29 16:19:09 +0530

    Update program1_5.c

--- Assignments/Assignment_1 /program1_5.c
+++ Assignments/Assignment_1 /program1_5.c
@@ -1,1 +1,26 @@
+//Assignment Error 
 
+#include <stdio.h>
+
+void Accept(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("*");
+    }
+}
+
+int main()
+//Question says to take input from user,but in 
+//assignment there was fixed value so i changed it to 
+//user input
+
+{
+   int iValue = 0;
+   printf("Enter No :");
+   scanf("%d",&iValue);
+   Accept(iValue);
+   return 0 ;
+    
+}

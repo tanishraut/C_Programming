6cbb730b46aff49e341073432c93c72ad8d3a070
Tanish Raut <165566146+tanishraut@users.noreply.github.com>
2025-10-29 15:20:16 +0530

    Update program1_3.c
    Question Error

--- Assignments/Assignment_1 /program1_3.c
+++ Assignments/Assignment_1 /program1_3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+void Display()
+{
+    int i = 0;
+    i = 5;
+        while(i>1)
+        {
+            printf("%d\n",i);
+            i --; // not possible to print 5 to 1 with i ++ 
+        }
 
+    {
+        printf("%d\n",i);
+
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
